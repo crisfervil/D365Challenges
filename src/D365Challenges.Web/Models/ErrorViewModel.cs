1e29887ebae49247c28dab08c4bac56d70486273
Cristhian Fernandez <crisfervil@gmail.com>
2018-09-15 22:25:41 +0100

    Changed Web Template

---
+++ src/D365Challenges.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace D365Challenges.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
